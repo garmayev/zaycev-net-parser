c6c41730c3a240dab73f59230d79f0b64827942e
garmayev <garmayev@yandex.ru>
2019-04-22 01:51:49 +0800

    Пиздим рекурсивно

--- content.js
+++ content.js
@@ -1,21 +1,65 @@
-var doc = document;
+var allArtists = '';
+function getData(item) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", item.getAttribute('data-url'), false);
+	xhr.send();
+	var result = {
+		url: JSON.parse(xhr.response).url,
+		artist: item.querySelector('div[itemprop=byArtist] a').innerHTML,
+		title: item.querySelector('div[itemprop=name] a').innerHTML,
+		duration: item.getAttribute('data-duration')
+	}
+	return result;
+}
+
+function getAllArtists(page, url = '/artist/letter-0.html') {
+	pna = url.split("/"),
+	pageName = pna.pop().replace(/\.html$/, "-more.html");
+	result = '';
+	console.log(pageName+'?page='+page);
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", '/artist/'+pageName+'?page='+page, false);
+	xhr.send();
+	parser = new DOMParser();
+	doc = parser.parseFromString(xhr.response, "text/html");
+	if ( (doc.querySelectorAll('.error').length == 0 ) && (page < 5) ) {
+		result = getAllArtists(page+1, url);
+		allArtists += result;
+		return xhr.response;
+	}
+}
+
 chrome.extension.onMessage.addListener(function(request, sender, callback){
-	if(request.cmd =='exec0'){ //выполнить
-		var links = document.querySelectorAll('.musicset-track-list__items div[itemprop="track"]');
-		var result = [];
-		for (var item in links) {
-			if (typeof links[item] === 'object') {
-				console.log(typeof links[item], links[item]);
-				var chr = new XMLHttpRequest();
-				chr.open('GET', links[item].getAttribute('data-url'), false);
-				chr.send();
-				result.push({
-									'url': JSON.parse(chr.response).url, 
-									'artist': links[item].querySelector('div[itemprop="byArtist"] a').innerHTML, 
-									'title': links[item].querySelector('div[itemprop="name"] a').innerHTML, 
-									'duration': links[item].getAttribute('data-duration') });
+	switch (request.cmd) {
+		case 'getAllOnPage': 
+			var links = document.querySelectorAll('.musicset-track-list__items div[itemprop="track"]');
+			var result = [];
+			for (var item in links) {
+				if (typeof links[item] === 'object') {
+					result.push(getData(links[item]));
+				}
 			}
-		}
-		callback(result);
+			callback(result);
+		break;
+		case 'getByArtist':
+			var letterList = document.querySelectorAll('a.alphabet__item.alphabet__link');
+			getAllArtists(1);
+			console.log(allArtists);
+			for (var letter in letterList) {
+			}
+		break;
 	}
 });
+var tracks = document.querySelectorAll('div[itemprop=track]');
+
+tracks.forEach(function (track, i, tracks) {
+	var button = document.createElement('div');
+	button.setAttribute('class', 'btn');
+	button.innerHTML = '+';
+	button.addEventListener('click', function (e) {
+		var result = getData(e.target.closest('div[itemprop=track]'));
+		console.log(result);
+		chrome.extension.sendMessage({type: 'ACTION_ADD', data: result});
+	});
+	track.appendChild(button);
+})
--- popup.js
+++ popup.js
@@ -1,20 +1,36 @@
+function toServer(data, callback) {
+	fetch('http://localhost/', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: data
+	}).then(function (response) {
+		return JSON.parse(response);
+	}).then(function (response) {
+		if (response.status == 'ok') {
+			alert(response.status+'\n'+response.message);
+		}
+	})
+}
+
 $(function () {
 	$('a.getTracks').on('click', function () {
 		chrome.tabs.getSelected(null, function(tab){
-			chrome.tabs.sendMessage(tab.id, {cmd: 'exec0', data: 'h'}, function (response) {
-				if( (response === undefined) || (response == '') || (response == ' ') ) {
-					$('.response').html('Empty response');
-				} else {
-					fetch('http://localhost/', {method: 'POST', headers: {'Accept': 'application/json', 'Content-Type': 'application/json', 'Access-Control-Request-Headers': '*', 'Access-Control-Request-Method': '*'}, body: JSON.stringify(response) }).then(function (responseText) {
-						console.log(responseText);
-					})
-					var result = "";
-					for (var i = 0; i<response.length; i++ ) {
-						result += '<a href="'+response[i]['data-url']+'">'+response[i]['artist']+' - '+response[i]['title']+'</a><br>';
-					}
-					$('.response').html(result);
-				}
+			chrome.tabs.sendMessage(tab.id, {cmd: 'getAllOnPage'}, function (response) {
+				toServer(JSON.stringify(response));
+			})
+		});
+	});
+	$('a.getByArtist').on('click', function () {
+		chrome.tabs.getSelected(null, function(tab){
+			chrome.tabs.sendMessage(tab.id, {cmd: 'getByArtist'}, function (response) {
+				toServer(JSON.stringify(response));
 			})
 		});
 	})
+})
+
+chrome.extension.onMessage.addListener(function (request, sender) {
+	toServer(JSON.stringify(request.data));
 })
